3fde4334a943b4b79aad016a7999462fb3c8f067
clayto-nr <claytonrennan01@gmail.com>
2024-05-19 23:12:40 -0300

    reload image

--- lib/screens/login.page.dart
+++ lib/screens/login.page.dart
@@ -75,7 +75,7 @@ class _LoginPageState extends State<LoginPage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Image.asset(
-                '../assets/images/logo.png',
+                'assets/images/logo.png',
                 width: 150,
                 height: 150,
               ),
